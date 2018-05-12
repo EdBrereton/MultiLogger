8e7c2a7e636009d9a6908b680e8e13dd25e8d8af
Ed Brereton <edbrereton@gmail.com>
2018-05-12 22:11:27 +0100

    Added some local logging options

---
+++ StdErr_Logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from LogLevels import LogLevel
+from LoggerAbc import Logger
+import datetime
+import sys
+
+class StdErr_Logger(Logger):
+    
+    def __init__ (self,logLocation, logSource):
+        self._logLocation_ = logLocation
+        self._logSource_ = logSource
+
+
+    def _writeLog_(self, logLevel, logMessage):
+        logLine = "[{0}]{1} - {2} - {3}\n".format(self._logSource_, datetime.datetime.now(), logLevel.name, logMessage)
+        print(logLine, file=sys.stderr)
+
+    def LogInfo(self, logMessage):
+        self._writeLog_(LogLevel.INFO, logMessage)
+
+    def LogWarn(self, logMessage):
+        self._writeLog_(LogLevel.WARN, logMessage)
+
+    def LogError(self, logMessage):
+        self._writeLog_(LogLevel.ERROR, logMessage)
+
+    def LogSecurity(self, logMessage):
+        self._writeLog_(LogLevel.SECURITY, logMessage)
+
+    def LogCritical(self, logMessage):
+        self._writeLog_(LogLevel.CRITICAL, logMessage)
