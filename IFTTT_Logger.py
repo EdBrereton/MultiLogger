e97eb78839ffacc74233a60570c55085a0137a91
Ed Brereton <edbrereton@gmail.com>
2018-05-12 21:42:56 +0100

    Add logging helper methods

--- IFTTT_Logger.py
+++ IFTTT_Logger.py
@@ -11,11 +11,26 @@ class IFTTT_Logger(object):
         self._iftttKey_ = iftttKey
         self._logSource_ = logSource
 
-    def WriteLog(self, logLevel, logMessage):
+    def _writeLog_(self, logLevel, logMessage):
         logUrl = "https://maker.ifttt.com/trigger/{0}/with/key/{1}".format(self._iftttMethod_, self._iftttKey_)
         logText = {"value1" : self._logSource_, "value2" : logLevel.name, "value3" : logMessage}
         payload = json.dumps(logText)
-        r = requests.post(url=logUrl, data=payload)
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(url=logUrl, data=payload, headers=headers)
         print(r.content)
     
+    def LogInfo(self, logMessage):
+        self._writeLog_(LogLevel.INFO, logMessage)
+
+    def LogWarn(self, logMessage):
+        self._writeLog_(LogLevel.WARN, logMessage)
+
+    def LogError(self, logMessage):
+        self._writeLog_(LogLevel.ERROR, logMessage)
+
+    def LogSecurity(self, logMessage):
+        self._writeLog_(LogLevel.SECURITY, logMessage)
+
+    def LogCritical(self, logMessage):
+        self._writeLog_(LogLevel.CRITICAL, logMessage)
     
